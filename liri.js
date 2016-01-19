edf264bae5f37ba67192bc94dda4996afb9cfdef
Mohammed Rizvi <morizvi07@gmail.com>
2016-01-18 22:20:09 -0500

    Added spotify function that shows song results from whatever i type in for the fourth item but it will be sliced [2], so it is really the first item in the array

--- liri.js
+++ liri.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-var keys = require("./key.js");
+var keys = require("./keys.js");
 var twitter = require("twitter");
 var spotify = require("spotify");
 var request = require("request");
@@ -22,4 +22,42 @@ switch(params[0]) {
     } else
       movieName("Mr. Nobody");
     break;
+}
+
+function twitterStatus() {
+  var client = new Twitter({
+    consumer_key: key.twitterKeys.consumer_key,
+    consumer_secret: key.twitterKeys.consumer_secret,
+    access_token_key: key.twitterKeys.access_token_key,
+    access_token_secret: key.twitterKeys.access_token_secret
+  });
+  var params = {screen_name: 'ur_mom06'};
+    client.get('statuses/user_timeline', params, function(error, tweets, response){
+    if (!error) {
+      console.log(data.tweets);
+    };
+  });
+};
+
+function spotifySong() {
+  spotify.search({ type: 'track', query: params[1] }, function(err, data) {
+    if ( err ) {
+      console.log('Error occurred: ' + err);
+      return;
+    } else {
+      for (var i = 0; i < data.tracks.items.length; i++) {
+        console.log("Song: " + data.tracks.items[i].name);
+        console.log("Artist: " + data.tracks.items[i].artists[0].name);
+        console.log("Album: "+ data.tracks.items[i].album.name);
+        console.log("Link: "+ data.tracks.items[i].preview_url);
+        console.log(" ");
+
+        fs.appendFile("log.txt", "Song: " + data.tracks.items[i].name + "\r\n", function(err) {
+          if(err) {
+          return console.log(err);
+          }
+        });
+      };
+    }
+  });
 }
